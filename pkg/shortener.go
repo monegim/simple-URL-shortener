e3738ce716917a2140af246a44159d41b276551f
Mostafa Negim <mostafa.negim@gmail.com>
2024-08-05 11:09:10 +0330

    Add: func boilerplates for shortener

--- pkg/shortener.go
+++ pkg/shortener.go
@@ -1,5 +1,14 @@
 package pkg
 
 func URLShortener(url string) string {
+	id := getIdFromDB(url)
+	shortenedString := id2String(id)
+	return shortenedString
+}
+
+func getIdFromDB(url string) uint64 {
+	return 0
+}
+func id2String(id uint64) string {
 	return ""
 }
